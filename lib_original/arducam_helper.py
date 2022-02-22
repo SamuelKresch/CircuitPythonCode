24471b82c4a7ded2c14f7275a689b5917d57a9b9
David Zhu <55730058+davidwuya@users.noreply.github.com>
2022-02-21 20:23:43 -0800

    lib update
    micropygps.mpy is already compiled

---
+++ lib_original/arducam_helper.py
@@ -0,0 +1,144 @@
+OV2640=0
+OV5642=1
+
+MAX_FIFO_SIZE=0x7FFFFF
+ARDUCHIP_FRAMES=0x01
+ARDUCHIP_TIM=0x03
+VSYNC_LEVEL_MASK=0x02
+ARDUCHIP_TRIG=0x41
+CAP_DONE_MASK=0x08
+
+OV5642_CHIPID_HIGH=0x300a
+OV5642_CHIPID_LOW=0x300b
+
+OV5642_320x240  =0
+OV5642_640x480  =1
+OV5642_1024x768 =2
+OV5642_1280x960 =3
+OV5642_1600x1200=4
+OV5642_2048x1536=5
+OV5642_2592x1944=6
+OV5642_1920x1080=7
+
+Advanced_AWB =0
+Simple_AWB   =1
+Manual_day   =2
+Manual_A     =3
+Manual_cwf   =4
+Manual_cloudy=5
+
+degree_180=0
+degree_150=1
+degree_120=2
+degree_90 =3
+degree_60 =4
+degree_30 =5
+degree_0  =6
+degree30  =7
+degree60  =8
+degree90  =9
+degree120 =10
+degree150 =11
+
+Auto   =0
+Sunny  =1
+Cloudy =2
+Office =3
+Home   =4
+
+Antique     =0
+Bluish      =1
+Greenish    =2
+Reddish     =3
+BW          =4
+Negative    =5
+BWnegative  =6
+Normal      =7
+Sepia       =8
+Overexposure=9
+Solarize    =10
+Blueish     =11
+Yellowish   =12
+
+Exposure_17_EV  =0
+Exposure_13_EV  =1
+Exposure_10_EV  =2
+Exposure_07_EV  =3
+Exposure_03_EV  =4
+Exposure_default=5
+Exposure03_EV   =6
+Exposure07_EV   =7
+Exposure10_EV   =8
+Exposure13_EV   =9
+Exposure17_EV   =10
+
+Auto_Sharpness_default=0
+Auto_Sharpness1       =1
+Auto_Sharpness2       =2
+Manual_Sharpnessoff   =3
+Manual_Sharpness1     =4
+Manual_Sharpness2     =5
+Manual_Sharpness3     =6
+Manual_Sharpness4     =7
+Manual_Sharpness5     =8
+
+MIRROR     =0
+FLIP       =1
+MIRROR_FLIP=2
+
+Saturation4 =0
+Saturation3 =1
+Saturation2 =2
+Saturation1 =3
+Saturation0 =4
+Saturation_1=5
+Saturation_2=6
+Saturation_3=7
+Saturation_4=8
+
+Brightness4 =0
+Brightness3 =1
+Brightness2 =2
+Brightness1 =3
+Brightness0 =4
+Brightness_1=5
+Brightness_2=6
+Brightness_3=7
+Brightness_4=8
+
+Contrast4 =0
+Contrast3 =1
+Contrast2 =2
+Contrast1 =3
+Contrast0 =4
+Contrast_1=5
+Contrast_2=6
+Contrast_3=7
+Contrast_4=8
+
+Antique      = 0
+Bluish       = 1
+Greenish     = 2
+Reddish      = 3
+BW           = 4
+Negative     = 5
+BWnegative   = 6
+Normal       = 7
+Sepia        = 8
+Overexposure = 9
+Solarize     = 10
+Blueish      = 11
+Yellowish    = 12
+
+high_quality   =0
+default_quality=1
+low_quality    =2
+
+Color_bar   =0
+Color_square=1
+BW_square   =2
+DLI         =3
+
+BMP =0
+JPEG=1
+RAW =2
